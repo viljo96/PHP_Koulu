068949da382b24c1c0c07632cf4f249888df45bf
viljo96 <43821343+viljo96@users.noreply.github.com>
2019-05-07 13:54:11 +0300

    Update poistarivi.php

--- poistarivi.php
+++ poistarivi.php
@@ -3,7 +3,7 @@
   $servername = "127.0.0.1:50300";
   $username = "azure";
   $password = "6#vWHD_$";
-  $dbname="liikuntakanta";
+  $dbname="localdb";
   // Luodaan tietokantayhteys
   $conn = new mysqli($servername, $username, $password,$dbname);
 
