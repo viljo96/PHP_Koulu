10f50848900c8d01712b34b0c00075ed1fa41df6
viljo96 <43821343+viljo96@users.noreply.github.com>
2019-05-07 14:28:57 +0300

    Update poistarivi.php

--- poistarivi.php
+++ poistarivi.php
@@ -7,8 +7,6 @@
   // Luodaan tietokantayhteys
   $conn = new mysqli($servername, $username, $password,$dbname);
 
-  $conn = new mysqli($servername, $username, $password, $dbname);
-
   $sql ="DELETE FROM liikunta WHERE RiviId =$napinID ";
   $result = $conn->query($sql);
       header("refresh:1;url=liikuntasuoritukset.php");
