ec1f8fdecec506e7a8b4c569409b073ed4c97498
viljo96 <43821343+viljo96@users.noreply.github.com>
2019-05-07 15:21:03 +0300

    Update poistarivi.php

--- poistarivi.php
+++ poistarivi.php
@@ -1,4 +1,5 @@
 <?php
+//Alustetaan muuttujat
   $napinID=$_POST['id'];
   $servername = "127.0.0.1:50300";
   $username = "azure";
@@ -6,7 +7,7 @@
   $dbname="localdb";
   // Luodaan tietokantayhteys
   $conn = new mysqli($servername, $username, $password,$dbname);
-
+//Poistetaan rivi tietokannasta
   $sql ="DELETE FROM liikunta WHERE RiviId =$napinID ";
   $result = $conn->query($sql);
       header("refresh:1;url=liikuntasuoritukset.php");
