201f1656eb31db6e0408d76b42a9c9b6792d7dd2
viljo96 <43821343+viljo96@users.noreply.github.com>
2019-05-07 14:26:53 +0300

    Update liikuntasuoritukset.php

--- liikuntasuoritukset.php
+++ liikuntasuoritukset.php
@@ -22,12 +22,6 @@ th{
 </head>
 <body>
 <?php
-function poistaRivi(){
-  $conn = new mysqli($servername, $username, $password, $dbname);
-
-  $sql ="DROP FROM liikunta WHERE";
-  $result = $conn->query($sql);
-}
 session_start();
 $servername = "127.0.0.1:50300";
 $username = "azure";
@@ -58,7 +52,7 @@ if ($result->num_rows > 0) {
     echo$th1."Kalorit".$th2;
     echo$row2.$br;
     while($row = $result->fetch_assoc()) {
-      echo$row1.$td1.$row["liikuntamuoto"].$td2.$td1.$row["Tunnit"].$td2.$td1.$row["Paivamaara"].$td2.$td1.$row["Kalorit"].$td2.$td1.$Button1.$td2.$row2;
+       echo$row1.$td1.$row["liikuntamuoto"].$td2.$td1.$row["Tunnit"].$td2.$td1.$row["Paivamaara"].$td2.$td1.$row["Kalorit"].$td2.$td1."<form method=\"POST\" action=\"/poistarivi.php\"><button type=\"submit\" name=\"id\" value=\"$row[RiviId]\">poista</button></form>".$td2.$row2;
     }
     echo $table2;
   }
